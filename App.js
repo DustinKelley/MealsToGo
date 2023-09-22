4bb477d1a6402e13a5b7f59f6d0aed2a8981b7d0
Dustin Kelley <dustin@saltandlight.community>
2023-09-22 08:52:55 -0500

    added restaurant components

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import * as React from "react";
-import { Searchbar } from "react-native-paper";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = React.useState(" ");
-  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-//This is a feature
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    // backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
